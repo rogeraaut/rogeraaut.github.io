8ea74b61b0cd3672a91ca0145089a03f08bb2440
rogera.dd <wolfgang.schuessel@gmail.com>
2017-11-09 04:45:36 +0100

    wp, testable

--- lang.java.d/tests/EinsTest.java
+++ lang.java.d/tests/EinsTest.java
@@ -1,14 +1,22 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by rogera on 6/17/17.
  */
-class EinsTest {
-    @org.junit.jupiter.api.Test
-    void getHello() {
+public class EinsTest {
+    @Test
+    public void getHelloTest() {
         String actual = (new Eins()).getHello();
         String expected = "Hello";
+        assertEquals(1, 1);
         assertEquals(expected, actual);
     }
 
+
+    @Test
+    public void testHello() {
+        assertEquals("Hello", new Eins().getHello());
+    }
 }
